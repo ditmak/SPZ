3e8c2e16763148ab092d758c04a1ecfc733d2148
chslzj <chslzj@163.com>
2015-03-06 17:27:03 +0800

    计算干支历年份

--- src/main/java/com/csl/util/date/TrunkBranchDate.java
+++ src/main/java/com/csl/util/date/TrunkBranchDate.java
@@ -1,26 +1,12 @@
 package com.csl.util.date;
 
-import java.util.Calendar;
 
 public class TrunkBranchDate {
-	private Trunk yearTrunk;
-	private Branch yearBranch;
-	private Trunk monthTrunk;
-	private Branch monthBranch;
-	private Trunk dayTrunk;
-	private Branch dayBranch;
-	private Trunk hourTrunk;
-	private Branch hourBranch;
-	public TrunkBranchDate(Calendar cal) {
-	//1，先判断是否过了立春，没过就以-1计算年份
-	
-	//2，根据月份判断是否过了该月的第一个节气，没过就减一
-	
-	//3,根据年份的结果和月份的结果判断月份的地支
-		
-	//4,算出该日干支
-		
-	//5,根据日子的干支时间计算时辰	
-	}
+	private static String[] TRUNKS = {"甲","乙","丙","丁","戊","己","庚","辛","壬","癸"};
+	private static String[] BRANCHES = {"子","丑","寅","卯","辰","巳","午","未","申","酉","戌","亥"};
+	public static String getTBYear(int year){
+		int offset = year-4;
+		return TRUNKS[offset%10]+BRANCHES[offset%12]+"年";
+	};
 
 }
