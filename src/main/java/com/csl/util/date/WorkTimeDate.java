fe28cfec3a16d0e9e6baa6c4ed4cb57eb4c4367f
chslzj <chslzj@163.com>
2015-03-16 18:35:30 +0800

    修复如果最后日期加上剩余时间越过周末计数不准的问题

--- src/main/java/com/csl/util/date/WorkTimeDate.java
+++ src/main/java/com/csl/util/date/WorkTimeDate.java
@@ -5,14 +5,17 @@ import org.joda.time.DateTime;
 public class WorkTimeDate {
 
 	public static DateTime plusWorkDays(DateTime time,int days) throws Exception{
+		//1.先计算可以有几个整周的日子
 		int weeks = days/5;
+		//剩余的日子
 		int day = days%5;
 		DateTime result = time.plusWeeks(weeks);
+		//如果几周过后是周末，调整到周一
 		while(result.getDayOfWeek()>5)
 			result = result.plusDays(1);
-		result = result.plusDays(day);
-		while(result.getDayOfWeek()>5)
-			result = result.plusDays(1);
+		while(day-->0){
+			result = result.plus(1);
+		}
 		return result;
 	}
 
